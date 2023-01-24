3f38c876ac7c7f6ae1d98f07ec73a2f67f4ab7f7
seljaks <33955366+seljaks@users.noreply.github.com>
2023-01-24 13:14:03 +0100

    cleanup

--- src/santa_2022/array.py
+++ src/santa_2022/array.py
@@ -3,7 +3,7 @@ from itertools import *
 
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    """powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
